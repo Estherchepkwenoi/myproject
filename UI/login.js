029124e07e6270b2df88bc055e99857d0e044c83
Estherchepkwenoi <chepkwenoiesther@gmail.com>
2018-10-13 23:52:12 +0300

    add changes

---
+++ UI/login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+const username = document.querySelector("username");
+const password = document.querySelector("password");
+
+form,addEventListener("LOGIN",(e)=>{e.preventDefault()
+
+    if(username.value !=="admin" && password !=="password")
+    window.location="./admin.html";
+    } 
+    else if(username.value !==)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
