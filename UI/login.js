4e50fcaf0025a98d7fae67fbc4194d2ea9e7670b
Estherchepkwenoi <chepkwenoiesther@gmail.com>
2018-10-14 21:27:33 +0300

    add changes

--- UI/login.js
+++ UI/login.js
@@ -1,14 +1,15 @@
-const form = document.getElementById("form");
+const form = document.getElementById("form-login");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-form.addEventListener("LOGIN",(event)=>{event.preventDefault();
+form.addEventListener("submit",(event)=>{event.preventDefault();
 
-    if(username.value =="admin" && password =="password"){
-    window.location="./admin.html";
-    
-      }else if(username.value =="attendant" && password =="password"){
-    window.location="./attendant.html";
+    if(username.value ==="admin" && password.value =="password"){
+    window.location= "admin.html";
+
+      
+    }else if(username.value ==="attendant" && password.value =="password"){
+    window.location="attendant.html";
 
       }else{
           alert("Invalid username or password");
